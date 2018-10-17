cfee99dbc41303c4e45be43ed6ac2a7485000d87
Kostas Kryarov <kvkryarov@avito.ru>
2018-10-17 20:35:59 +0300

    Added generated words

---
+++ utils/generate-md.js
@@ -0,0 +1,24 @@
+require('babel-register')({
+    presets: [ 'env' ]
+});
+
+const fs = require('fs');
+const themes = require('../src/constants/themes').default;
+
+const md = themes
+    .map(theme => {
+        return [
+            `#### ${theme.title}`,
+            '| ------ | ------ |',
+            ...theme.words.map(word => {
+                return `| \`${word.japanese}\` | ${word.translation} |`;
+            })
+        ].join('\n');
+    })
+    .join('\n')
+
+fs.writeFile('WORDS.md', md, 'utf-8', err => {
+    if (err) {
+        throw err;
+    }
+}); 
